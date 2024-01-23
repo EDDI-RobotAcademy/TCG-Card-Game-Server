8ff1cd5eaf209cade6c471406bb8cee44298dc88
이형준 <gggudwnsgg@naver.com>
2024-01-23 17:14:55 +0900

    update: 카드 race,grade entity 재수정 [TCG-CGS-43]

--- src/card_race/entity/card_race.rs
+++ src/card_race/entity/card_race.rs
@@ -1,9 +1,5 @@
 use std::fmt;
 
 pub struct Cardrace {
-    pub angel:  i32,            //천사 휴먼 트렌트 언데트 기계 순
-    pub human: i32,
-    pub trent: i32,
-    pub undead: i32,
-    pub mechanic: i32,
+    pub card_race:  i32,            //천사 휴먼 트렌트 언데트 기계 순 //angel human trent undead mechanic
 }
