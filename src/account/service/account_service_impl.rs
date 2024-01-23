d5ba96c95945c6ab19a2cb48f46c34c0aff235d8
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-01-23 12:25:36 +0900

    update: [박장훈] 회원 가입 중복 확인 로직 구현 [TCG-CGS-21]

--- src/account/service/account_service_impl.rs
+++ src/account/service/account_service_impl.rs
@@ -57,27 +57,19 @@ impl AccountService for AccountServiceImpl {
 
         let account_repository = self.repository.lock().await;
         let account = account_register_request.to_account().unwrap();
-        println!("중복 확인 시작");
+        // 중복 확인 작업
         if account_repository.find_by_user_id(account.user_id()).await.unwrap().is_some() {
-            println!("중복된 아이디 존재");
+            // 중복 아이디 존재하는 경우
             AccountRegisterResponse::new(false)
-        } else if account_repository.find_by_user_id(account.user_id()).await.unwrap().is_none() {
-            println!("중복된 아이디 존재하지 않아 계정 저장 시작");
+        } else {
+            // 중복 아이디 존재하지 않는 경우 계정 저장
             let result = account_repository.save(account_register_request.to_account().unwrap()).await;
             if result.is_ok() {
-                    println!("계정 저장 완료");
                     AccountRegisterResponse::new(true)
                 } else {
-                println!("계정 저장 실패");
                 AccountRegisterResponse::new(false)
             }
-        } else {
-            println!("중복 확인 자체 실패");
-            AccountRegisterResponse::new(false)
         }
-        // if result.is_ok() {
-        //     AccountRegisterResponse::new(true)
-        // }
     }
 
     async fn account_login(&self, account_login_request: AccountLoginRequest) -> AccountLoginResponse {
