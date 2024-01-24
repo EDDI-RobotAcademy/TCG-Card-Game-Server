ca2b66f4aa8aa4a0d4f1aeaae602e210a6fd9ec8
Sanghoon Lee <gcccompil3r@gmail.com>
2024-01-24 17:52:14 +0900

    fix: [이상훈] 배틀 매칭 API 오류 해결 [TCG-CGS-57]

--- src/battle_ready_monitor/repository/battle_ready_monitor_repository.rs
+++ src/battle_ready_monitor/repository/battle_ready_monitor_repository.rs
@@ -5,5 +5,5 @@ use crate::battle_ready_monitor::entity::battle_ready_status::BattleReadyStatus;
 #[async_trait]
 pub trait BattleReadyMonitorRepository {
     async fn save_battle_account_hash(&mut self, accounts_vector: Vec<i32>, state: BattleReadyStatus);
-    async fn get_account_status(&mut self, account_unique_id: i32) -> Result<BattleReadyStatus, Box<dyn Error>>;
+    async fn get_account_status(&mut self, account_unique_id: i32) -> BattleReadyStatus;
 }
