46008e337de4949c6a22d584020e30d2576ee75e
Sanghoon Lee <gcccompil3r@gmail.com>
2024-01-24 16:19:41 +0900

    feat: [이상훈] 플레이어간 배틀 매칭을 위한 Wait Queue 모니터링 Task 구동 [TCG-CGS-56]

--- src/battle_ready_monitor/controller/battle_ready_monitor_controller_impl.rs
+++ src/battle_ready_monitor/controller/battle_ready_monitor_controller_impl.rs
@@ -1,27 +1,43 @@
 use std::sync::Arc;
+use async_trait::async_trait;
 use lazy_static::lazy_static;
+use tokio::sync::Mutex as AsyncMutex;
+
+use crate::battle_ready_monitor::controller::battle_ready_monitor_controller::BattleReadyMonitorController;
+use crate::battle_ready_monitor::service::battle_ready_monitor_service::BattleReadyMonitorService;
 use crate::battle_ready_monitor::service::battle_ready_monitor_service_impl::BattleReadyMonitorServiceImpl;
 
 pub struct BattleReadyMonitorControllerImpl {
-    battle_ready_monitor_service: Arc<BattleReadyMonitorServiceImpl>,
+    battle_ready_monitor_service: Arc<AsyncMutex<BattleReadyMonitorServiceImpl>>,
 }
 
 impl BattleReadyMonitorControllerImpl {
     pub fn new(battle_ready_monitor_service:
-               Arc<BattleReadyMonitorServiceImpl>) -> Self {
+               Arc<AsyncMutex<BattleReadyMonitorServiceImpl>>) -> Self {
 
         BattleReadyMonitorControllerImpl {
             battle_ready_monitor_service,
         }
     }
 
-    pub fn get_instance() -> Arc<BattleReadyMonitorControllerImpl> {
+    pub fn get_instance() -> Arc<AsyncMutex<BattleReadyMonitorControllerImpl>> {
         lazy_static! {
-            static ref INSTANCE: Arc<BattleReadyMonitorControllerImpl> =
+            static ref INSTANCE: Arc<AsyncMutex<BattleReadyMonitorControllerImpl>> =
                 Arc::new(
-                    BattleReadyMonitorControllerImpl::new(
-                        BattleReadyMonitorServiceImpl::get_instance()));
+                    AsyncMutex::new(
+                        BattleReadyMonitorControllerImpl::new(
+                            BattleReadyMonitorServiceImpl::get_instance())));
         }
         INSTANCE.clone()
+    }
+}
+
+#[async_trait]
+impl BattleReadyMonitorController for BattleReadyMonitorControllerImpl {
+    async fn start_monitor_for_battle_match(&mut self) {
+        println!("BattleReadyMonitorControllerImpl: start_monitor_for_battle_match()");
+        let battle_ready_monitor_service_mutex = &self.battle_ready_monitor_service;
+        let mut battle_ready_monitor_service_guard = battle_ready_monitor_service_mutex.lock().await;
+        battle_ready_monitor_service_guard.start_monitor().await;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::Arc;
 use std::time::Duration;
+use crate::battle_ready_monitor::controller::battle_ready_monitor_controller::BattleReadyMonitorController;
+use crate::battle_ready_monitor::controller::battle_ready_monitor_controller_impl::BattleReadyMonitorControllerImpl;
 use crate::client_socket_accept::controller::client_socket_accept_controller::ClientSocketAcceptController;
 use crate::client_socket_accept::controller::client_socket_accept_controller_impl::ClientSocketAcceptControllerImpl;
 use crate::common::env::env_detector::EnvDetector;
@@ -96,6 +99,18 @@ async fn main() {
 
     thread_worker_service_guard.save_async_thread_worker("Transmitter", Box::new(receiver_function.clone()));
     thread_worker_service_guard.start_thread_worker("Transmitter").await;
+
+    let battle_ready_monitor_function = || -> Pin<Box<dyn Future<Output = ()> + Send>> {
+        Box::pin(async {
+            let battle_ready_monitor_controller_mutex = BattleReadyMonitorControllerImpl::get_instance();
+            let mut battle_ready_monitor_guard = battle_ready_monitor_controller_mutex.lock().await;
+            println!("Battle Ready Monitor instance found. Executing transmit_to_client().");
+            let _ = battle_ready_monitor_guard.start_monitor_for_battle_match().await;
+        })
+    };
+
+    thread_worker_service_guard.save_async_thread_worker("BattleReadyMonitor", Box::new(battle_ready_monitor_function.clone()));
+    thread_worker_service_guard.start_thread_worker("BattleReadyMonitor").await;
 
     loop {
         tokio::time::sleep(Duration::from_secs(10)).await;
