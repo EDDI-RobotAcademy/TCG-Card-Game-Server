d4d4abd83b78db616354dc53f748bcd7c6fe7597
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-01-24 12:43:14 +0900

    update: [박장훈] 덱 생성 요청에 대한 response type 지정 [TCG-CGS-41]

--- src/response_generator/response_type.rs
+++ src/response_generator/response_type.rs
@@ -1,6 +1,7 @@
 use crate::account::service::response::account_register_response::AccountRegisterResponse;
 use serde::{Deserialize, Serialize};
 use crate::account::service::response::account_login_response::AccountLoginResponse;
+use crate::account_deck::service::response::account_deck_register_response::AccountDeckRegisterResponse;
 use crate::battle_room::service::response::battle_match_response::BattleMatchResponse;
 use crate::client_program::service::response::client_program_exit_response::ClientProgramExitResponse;
 
@@ -8,6 +9,8 @@ use crate::client_program::service::response::client_program_exit_response::Clie
 pub enum ResponseType {
     ACCOUNT_REGISTER(AccountRegisterResponse),
     ACCOUNT_LOGIN(AccountLoginResponse),
+
+    ACCOUNT_DECK_REGISTER(AccountDeckRegisterResponse),
 
     BATTLE_MATCH(BattleMatchResponse),
 
