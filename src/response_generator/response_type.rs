26816b91e4c1a2791f55f95e7aa80087120810be
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-01-25 17:22:25 +0900

    update: [박장훈] 덱 수정 요청에 대한 response type 지정 [TCG-CGS-41]

--- src/response_generator/response_type.rs
+++ src/response_generator/response_type.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::account::service::response::account_login_response::AccountLoginResponse;
 use crate::account::service::response::account_logout_response::AccountLogoutResponse;
 use crate::account_deck::service::response::account_deck_list_response::AccountDeckListResponse;
+use crate::account_deck::service::response::account_deck_modify_response::AccountDeckModifyResponse;
 use crate::account_deck::service::response::account_deck_register_response::AccountDeckRegisterResponse;
 use crate::battle_ready_monitor::service::response::battle_ready_response::BattleReadyResponse;
 use crate::battle_room::service::response::battle_match_response::BattleMatchResponse;
@@ -17,6 +18,7 @@ pub enum ResponseType {
 
     ACCOUNT_DECK_REGISTER(AccountDeckRegisterResponse),
     ACCOUNT_DECK_LIST(AccountDeckListResponse),
+    ACCOUNT_DECK_MODIFY(AccountDeckModifyResponse),
 
     BATTLE_MATCH(BattleMatchResponse),
     BATTLE_READY(BattleReadyResponse),
