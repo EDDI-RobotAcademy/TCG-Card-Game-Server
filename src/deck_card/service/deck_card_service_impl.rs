ad2fbf2ccd99dcf92696d3da81121b6aa084d2b9
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-01-26 16:28:46 +0900

    feat: [박장훈] 덱 카드 리스트 불러오기 기능 구현 [TCG-CGS-82]

--- src/deck_card/service/deck_card_service_impl.rs
+++ src/deck_card/service/deck_card_service_impl.rs
@@ -6,7 +6,9 @@ use crate::deck_card::repository::deck_card_repository::DeckCardRepository;
 
 use crate::deck_card::repository::deck_card_repository_impl::DeckCardRepositoryImpl;
 use crate::deck_card::service::deck_card_service::DeckCardService;
+use crate::deck_card::service::request::deck_card_list_request::DeckCardListRequest;
 use crate::deck_card::service::request::deck_configuration_request::DeckConfigurationRequest;
+use crate::deck_card::service::response::deck_card_list_response::DeckCardListResponse;
 use crate::deck_card::service::response::deck_configuration_response::DeckConfigurationResponse;
 
 pub struct DeckCardServiceImpl {
@@ -34,6 +36,8 @@ impl DeckCardServiceImpl {
 #[async_trait]
 impl DeckCardService for DeckCardServiceImpl {
     async fn deck_configuration_register(&self, deck_configuration_request: DeckConfigurationRequest) -> DeckConfigurationResponse {
+        println!("DeckCardServiceImpl: deck_configuration_register()");
+
         let deck_card_repository = self.repository.lock().await;
         let result = deck_card_repository.save(deck_configuration_request).await;
         match result {
@@ -42,6 +46,23 @@ impl DeckCardService for DeckCardServiceImpl {
             }
             Err(error_message) => {
                 DeckConfigurationResponse::new(false, error_message)
+            }
+        }
+    }
+    async fn deck_card_list(&self, deck_card_list_request: DeckCardListRequest) -> DeckCardListResponse {
+        println!("DeckCardServiceImpl: deck_card_list()");
+
+        let deck_card_repository = self.repository.lock().await;
+        let deck_id_i32 = deck_card_list_request.deck_id();
+        let result = deck_card_repository.get_card_list(deck_id_i32).await;
+        match result {
+            Ok(opt_list) => {
+                let card_id_count_list = opt_list.unwrap();
+                DeckCardListResponse::new(card_id_count_list)
+            }
+            Err(e) => {
+                let empty_list = Vec::new();
+                DeckCardListResponse::new(empty_list)
             }
         }
     }
@@ -63,8 +84,9 @@ mod test {
         //     8, 8, 9, 9, 9, 11, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 15, 16, 16, 16, 17, 17, 17];
         // let card_id_list_too_many_duplicated_cards = [1, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7,
         //     8, 8, 9, 9, 9, 11, 11, 11, 12, 12, 12];
-        let test_card_id_list = [1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7,
-            8, 8, 9, 9, 9, 11, 11, 11, 12, 12, 12];
+        let test_card_id_list
+            = [1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 9,
+            9, 9, 11, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 15, 16, 17, 18];
 
         let mut card_vec = Vec::new();
         for id in test_card_id_list {
@@ -75,6 +97,16 @@ mod test {
         let result = deck_card_service_mutex_guard.deck_configuration_register(deck_config_request).await;
         println!("is_success: {}", result.get_is_success());
         println!("message: {}", result.get_message());
+    }
+    #[tokio::test]
+    async fn test_deck_card_list() {
+        let deck_card_service_mutex = DeckCardServiceImpl::get_instance();
+        let deck_card_service_mutex_guard = deck_card_service_mutex.lock().await;
+
+        let deck_card_list_request = DeckCardListRequest::new(8);
+
+        let result = deck_card_service_mutex_guard.deck_card_list(deck_card_list_request).await;
+        println!("{:?}", result.get_card_id_list());
     }
 
     #[tokio::test]
