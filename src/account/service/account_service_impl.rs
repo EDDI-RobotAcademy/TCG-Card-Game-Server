023b51dec982cfd84a0acab9586b9be7b43b4353
bikjo99 <149447229+bikjo99@users.noreply.github.com>
2024-01-26 12:07:11 +0900

    feat: [이성용] 계정삭제요청을 수신하면 계정을 삭제합니다. [TCG-CGS-61]

--- src/account/service/account_service_impl.rs
+++ src/account/service/account_service_impl.rs
@@ -197,17 +197,17 @@ async fn test_account_delete_in_account_service() {
 
     let test_id = "test_account_delete";
     let test_pw = "test_account_delete";
-    let hashed_password = hash(test_pw, 12);
+    let string_test_pw = test_pw.to_string();
 
     //test_account 저장
-    // let test_account = AccountRegisterRequest::new(test_id, hashed_password.expect("e"));
+    // let test_account = AccountRegisterRequest::new(test_id, string_test_pw);
     // account_service_gaurd.account_register(test_account).await;
 
     //test_account 로그인
-    let test_account = AccountLoginRequest::new(test_id, hashed_password.expect("e"));
-    account_service_gaurd.account_login(test_account).await;
+    // let test_account = AccountLoginRequest::new(test_id, string_test_pw);
+    // account_service_gaurd.account_login(test_account).await;
 
     //test_account 삭제
-    // let test_account = AccountDeleteRequest::new(test_id, hashed_password.expect("e"));
-    // account_service_gaurd.account_delete(test_account).await;
+    let test_account = AccountDeleteRequest::new(test_id, string_test_pw);
+    account_service_gaurd.account_delete(test_account).await;
 }
