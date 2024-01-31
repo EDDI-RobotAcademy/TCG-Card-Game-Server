e9327fc3d4085bcf17ecef6875a5286e14be5e4e
Sanghoon Lee <gcccompil3r@gmail.com>
2024-01-31 14:10:51 +0900

    feat: [이상훈] 1 vs 1 배틀 매칭 성사 시 플레이어가 사용할 무덤 객체 생성 [TCG-CGS-126]

---
+++ src/game_tomb/repository/game_tomb_repository_impl.rs
@@ -0,0 +1,71 @@
+use std::sync::Arc;
+use indexmap::IndexMap;
+use lazy_static::lazy_static;
+
+use tokio::sync::Mutex as AsyncMutex;
+
+use crate::game_tomb::entity::game_tomb::GameTomb;
+use crate::game_tomb::repository::game_tomb_repository::GameTombRepository;
+
+pub struct GameTombRepositoryImpl {
+    game_tomb_map: IndexMap<i32, GameTomb>,
+}
+
+impl GameTombRepositoryImpl {
+    pub fn new() -> Self {
+        GameTombRepositoryImpl {
+            game_tomb_map: IndexMap::new(),
+        }
+    }
+
+    pub(crate) fn get_game_tomb_map(&mut self) -> &mut IndexMap<i32, GameTomb> {
+        &mut self.game_tomb_map
+    }
+
+    pub fn get_instance() -> Arc<AsyncMutex<GameTombRepositoryImpl>> {
+        lazy_static! {
+            static ref INSTANCE: Arc<AsyncMutex<GameTombRepositoryImpl>> =
+                Arc::new(
+                    AsyncMutex::new(
+                        GameTombRepositoryImpl::new()));
+        }
+        INSTANCE.clone()
+    }
+}
+
+impl GameTombRepository for GameTombRepositoryImpl {
+    fn create_game_tomb_object(&mut self, account_unique_id: i32) -> bool {
+        println!("GameTombRepositoryImpl: create_game_tomb_object()");
+
+        let new_game_tomb_map = GameTomb::new();
+        self.game_tomb_map.insert(account_unique_id, new_game_tomb_map);
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio::test;
+
+    #[test]
+    async fn test_create_game_tomb_object() {
+        let mut game_tomb_repository = GameTombRepositoryImpl::new();
+        let result = game_tomb_repository.create_game_tomb_object(1);
+
+        assert!(result);
+
+        println!("Test Output: {:?}", game_tomb_repository.get_game_tomb_map());
+    }
+
+    #[tokio::test]
+    async fn test_get_instance() {
+        let instance = GameTombRepositoryImpl::get_instance();
+
+        let lock = instance.lock().await;
+
+        assert!(Arc::strong_count(&instance) > 1);
+        assert_eq!(lock.game_tomb_map.len(), 0);
+    }
+}
---
+++ src/game_tomb/repository/mod.rs
@@ -0,0 +1,2 @@
+pub mod game_tomb_repository;
+pub mod game_tomb_repository_impl;
