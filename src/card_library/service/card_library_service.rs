2f8077c034635d77106efe09e360c9e377f56f76
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-02-01 19:09:27 +0900

    update: [박장훈] card library 자원을 안전하게 공유할 수 있습니다 [TCG-CGS-124]

--- src/card_library/service/card_library_service.rs
+++ src/card_library/service/card_library_service.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use async_trait::async_trait;
+use crate::card_library::entity::card_dictionary_label::CardDictionaryLabel;
 
 #[async_trait]
 pub trait CardLibraryService {
-    async fn open_library(&self);
+    async fn open_library(&self, file_path: &str);
+    async fn get_dictionary(&self, label: CardDictionaryLabel) -> HashMap<String, String>;
 }
