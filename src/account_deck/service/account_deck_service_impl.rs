41168d05c4efba1aa1551f3cf8110715e3b19d13
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-02-03 01:26:40 +0900

    update: [박장훈] 덱 생성 개수 제한 6개로 제한 기능 추가 [TCG-CGS-50]

--- src/account_deck/service/account_deck_service_impl.rs
+++ src/account_deck/service/account_deck_service_impl.rs
@@ -42,6 +42,12 @@ impl AccountDeckServiceImpl {
         }
         INSTANCE.clone()
     }
+    async fn get_account_unique_id(&self, account_session_id: &str) -> i32 {
+        let mut redis_in_memory_repository = self.redis_in_memory_repository.lock().await;
+        let account_unique_id_option_string = redis_in_memory_repository.get(account_session_id).await;
+        let account_unique_id_string = account_unique_id_option_string.unwrap();
+        account_unique_id_string.parse().expect("Failed to parse account_unique_id_string as i32")
+    }
 }
 
 #[async_trait]
@@ -49,74 +55,57 @@ impl AccountDeckService for AccountDeckServiceImpl {
     async fn account_deck_register(&self, account_deck_register_request: AccountDeckRegisterRequest) -> AccountDeckRegisterResponse {
         println!("AccountDeckServiceImpl: account_deck_register()");
 
-        let account_deck_repository = self.repository.lock().await;
-        let mut redis_repository_guard = self.redis_in_memory_repository.lock().await;
-        let account_number_str = redis_repository_guard.get(account_deck_register_request.account_id()).await;
-        let account_unique_id: Result<i32, _> = account_number_str.expect("REASON").parse();
-        match account_unique_id {
-            Ok(int_type_account_id) => {
-                let account_deck = AccountDeck::new(int_type_account_id, account_deck_register_request.deck_name());
-                match account_deck {
-                    Ok(new_deck) => {
-                        let result = account_deck_repository.save(new_deck).await;
-                        AccountDeckRegisterResponse::new(true)
-                    }
-                    Err(e) => {
-                        AccountDeckRegisterResponse::new(false)
-                    }
-                }
-            }
-            Err(e) => {
-                AccountDeckRegisterResponse::new(false)
+        let account_unique_id = self.get_account_unique_id(account_deck_register_request.account_id()).await;
+        let account_deck = AccountDeck::new(account_unique_id, account_deck_register_request.deck_name()).unwrap();
+
+        let account_deck_repository_guard = self.repository.lock().await;
+
+        let account_deck_count_limit = 6;
+        if let Some(account_deck_list) =
+            account_deck_repository_guard.get_list_by_user_int_id(account_unique_id).await.unwrap() {
+            if account_deck_list.len() >= account_deck_count_limit {
+                return AccountDeckRegisterResponse::new(false)
             }
         }
-        // TODO: 덱이 계정당 최대 6개만 생성되어야 하므로, 계정의 덱 개수를 카운팅하는 작업 필요
+
+        let account_deck_save_result = account_deck_repository_guard.save(account_deck).await;
+
+        drop(account_deck_repository_guard);
+
+        if account_deck_save_result.is_err() { return AccountDeckRegisterResponse::new(false) }
+
+        AccountDeckRegisterResponse::new(true)
     }
 
     async fn account_deck_list(&self, account_deck_list_request: AccountDeckListRequest) -> AccountDeckListResponse {
         println!("AccountDeckServiceImpl: account_deck_list()");
 
-        let account_deck_repository = self.repository.lock().await;
-        let mut redis_repository_guard = self.redis_in_memory_repository.lock().await;
-        let account_number_str = redis_repository_guard.get(account_deck_list_request.account_id()).await;
-        let account_unique_id: Result<i32, _> = account_number_str.expect("REASON").parse();
-        match account_unique_id {
-            Ok(int_type_account_id) => {
-                if let Some(deck_list) = account_deck_repository.get_list_by_user_int_id(int_type_account_id).await.unwrap() {
-                    AccountDeckListResponse::new(deck_list)
-                } else {
-                    let empty_set = Vec::new();
-                    AccountDeckListResponse::new(empty_set)
-                }
-            }
-            Err(e) => {
-                println!("Deck list loading error : {}", e);
-                let empty_set = Vec::new();
-                AccountDeckListResponse::new(empty_set)
-            }
+        let account_unique_id = self.get_account_unique_id(account_deck_list_request.account_id()).await;
+
+        let account_deck_repository_guard = self.repository.lock().await;
+
+        if let Some(deck_list) =
+            account_deck_repository_guard.get_list_by_user_int_id(account_unique_id).await.unwrap() {
+            return AccountDeckListResponse::new(deck_list)
+        } else {
+            let empty_set = Vec::new();
+            AccountDeckListResponse::new(empty_set)
         }
     }
 
     async fn account_deck_modify(&self, account_deck_modify_request: AccountDeckModifyRequest) -> AccountDeckModifyResponse {
         println!("AccountDeckServiceImpl: account_deck_modify()");
 
-        let account_deck_repository = self.repository.lock().await;
-        let mut redis_repository_guard = self.redis_in_memory_repository.lock().await;
-        let account_number_str = redis_repository_guard.get(account_deck_modify_request.account_id()).await;
-        let account_unique_id: Result<i32, _> = account_number_str.expect("REASON").parse();
-        match account_unique_id {
-            Ok(int_id) => {
-                let result = account_deck_repository.update_data(account_deck_modify_request, int_id).await;
-                if result.is_ok() {
-                    AccountDeckModifyResponse::new(true)
-                } else {
-                    AccountDeckModifyResponse::new(false)
-                }
-            }
-            Err(e) => {
-                eprintln!("Error to get int id : {}", e);
-                AccountDeckModifyResponse::new(false)
-            }
+        let account_unique_id = self.get_account_unique_id(account_deck_modify_request.account_id()).await;
+
+        let account_deck_repository_guard = self.repository.lock().await;
+
+        let result = account_deck_repository_guard.update_data(account_deck_modify_request, account_unique_id).await;
+
+        if result.is_ok() {
+            AccountDeckModifyResponse::new(true)
+        } else {
+            AccountDeckModifyResponse::new(false)
         }
     }
 }
@@ -125,7 +114,6 @@ impl AccountDeckService for AccountDeckServiceImpl {
 mod tests {
     use super::*;
     use tokio::test;
-    use crate::redis::repository::redis_in_memory_repository_impl::RedisInMemoryRepositoryImpl;
 
     #[test]
     async fn test_deck_registration() {
@@ -133,13 +121,13 @@ mod tests {
         let mut account_deck_service_mutex_guard = account_deck_service_mutex.lock().await;
 
         let redis_token_str = "redis_token_str";
-        let sample_deck_name = "휴먼덱 화이팅";
+        let sample_deck_name = "7th deck";
 
         let account_deck_register_request = AccountDeckRegisterRequest::new(redis_token_str.to_string(), sample_deck_name.to_string());
 
         let result = account_deck_service_mutex_guard.account_deck_register(account_deck_register_request).await;
 
-        assert_eq!(true, result.get_is_success());
+        assert_eq!(false, result.get_is_success());
     }
 
     #[test]
@@ -159,7 +147,7 @@ mod tests {
 
         let sample_deck_id: i32 = 8;
         let sample_account_id_str = "redis_token_str";
-        let sample_deck_name = "new deck name";
+        let sample_deck_name = "amazing deck name";
 
         let account_deck_modify_request
             = AccountDeckModifyRequest::new(sample_deck_id, sample_account_id_str.to_string(), sample_deck_name.to_string());
