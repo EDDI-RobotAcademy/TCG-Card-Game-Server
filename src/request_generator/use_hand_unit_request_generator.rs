a02249337f5bdbd7b55f689be73a4db476a693e8
silenc3502 <gcccompil3r@gmail.com>
2024-02-04 19:28:41 +0900

    update: [이상훈] 배틀 필드 유닛 배치 request 인수값 변경 [TCG-CGS-55]

--- src/request_generator/use_hand_unit_request_generator.rs
+++ src/request_generator/use_hand_unit_request_generator.rs
@@ -2,11 +2,11 @@ use serde_json::Value as JsonValue;
 use crate::game_hand::service::request::use_game_hand_unit_card_request::UseGameHandUnitCardRequest;
 
 pub fn create_use_hand_unit_request(data: &JsonValue) -> Option<UseGameHandUnitCardRequest> {
-    if let (Some(deck_id), Some(session_info)) = (
-        data.get("deckId").and_then(|v| v.as_str()),
+    if let (Some(unit_number), Some(session_info)) = (
+        data.get("unitId").and_then(|v| v.as_str()),
         data.get("sessionInfo").and_then(|v| v.as_str())
     ) {
-        Some(UseGameHandUnitCardRequest::new(deck_id.to_string(), session_info.to_string()))
+        Some(UseGameHandUnitCardRequest::new(session_info.to_string(), unit_number.to_string()))
     } else {
         None
     }
