7c0bc5b0ac29a29bfcf399af6e6e28bab96f499a
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2024-02-05 10:14:15 +0900

    update:[황영찬] 클라이언트에서 카드 뽑기 요청을 수신하면 수신 된 데이터를 RequestForm 형태로 생성합니다 [TCG-CGS-20]

--- src/shop/service/request/get_card_default_request.rs
+++ src/shop/service/request/get_card_default_request.rs
@@ -1,11 +1,13 @@
 #[derive(Debug)]
 pub struct GetCardDefaultRequest {
     account_id: String,
+    race_name: String,
 }
 
 impl GetCardDefaultRequest {
-    pub fn new(account_id: String) -> Self {
-        GetCardDefaultRequest { account_id: account_id.to_string() }
+    pub fn new(account_id: String, race_name: String) -> Self {
+        GetCardDefaultRequest { account_id: account_id.to_string(), race_name: race_name.to_string() }
     }
     pub fn account_id(&self) -> &str { &self.account_id }
+    pub fn race_name(&self) -> &str { &self.race_name }
 }
