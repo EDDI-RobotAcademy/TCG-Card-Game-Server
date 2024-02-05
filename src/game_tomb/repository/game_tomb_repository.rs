e8be2691b34ed76d85ab893c596e2c7b7ffef192
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-02-05 14:59:32 +0900

    update: [박장훈] 에너지 카드 사용 후 묘지로 보내지는 로직 추가 [TCG-CGS-164]

--- src/game_tomb/repository/game_tomb_repository.rs
+++ src/game_tomb/repository/game_tomb_repository.rs
@@ -1,3 +1,4 @@
 pub trait GameTombRepository {
     fn create_game_tomb_object(&mut self, account_unique_id: i32) -> bool;
+    fn add_used_card_to_tomb(&mut self, account_unique_id: i32, used_card_id: i32);
 }
