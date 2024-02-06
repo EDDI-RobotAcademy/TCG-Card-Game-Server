d9f05616007fd68e247580701b033227b4d1490a
Sanghoon Lee <gcccompil3r@gmail.com>
2024-02-06 16:40:00 +0900

    feat: [이상훈] 시스템 내부에서 배틀룸에 있는 상대방의 고유 id 값을 찾을 수 있습니다 [TCG-CGS-189]

--- src/battle_room/repository/battle_room_repository.rs
+++ src/battle_room/repository/battle_room_repository.rs
@@ -7,4 +7,5 @@ use crate::battle_room::service::response::what_is_the_room_number_response::Wha
 pub trait BattleRoomRepository {
     async fn set_players_to_battle_room(&self, account_unique_id_list: Vec<i32>) -> Result<bool, Box<dyn Error>>;
     async fn what_is_the_room_number(&self, account_unique_id: i32) -> Option<i32>;
+    async fn find_opponent_unique_id(&self, account_unique_id: i32) -> Option<i32>;
 }
