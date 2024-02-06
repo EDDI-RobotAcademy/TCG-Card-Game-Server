f07c042e5d67e79ad7c4a004c1bf085f34934b1f
Sanghoon Lee <gcccompil3r@gmail.com>
2024-02-06 15:15:12 +0900

    update: [이상훈] 더미 추가 [TCG-CGS-187]

--- src/common/card_attributes/card_grade/card_grade_enum.rs
+++ src/common/card_attributes/card_grade/card_grade_enum.rs
@@ -1,5 +1,6 @@
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub enum GradeEnum {
+    Dummy = 0,
     Common = 1,
     Uncommon = 2,
     Hero = 3,
--- src/common/card_attributes/card_race/card_race_enum.rs
+++ src/common/card_attributes/card_race/card_race_enum.rs
@@ -1,5 +1,6 @@
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub enum RaceEnum {
+    Dummy = 0,
     Undead = 1,
     Human = 2,
     Trent = 3,
