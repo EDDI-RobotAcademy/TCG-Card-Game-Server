99e18e6be6a1084b5f64ecaf9dea09e3df33e8a5
silenc3502 <gcccompil3r@gmail.com>
2024-02-06 22:22:23 +0900

    feat: [이상훈] 넘치는 사기를 통해 언데드 유닛에게 에너지 2개 부스팅 [TCG-CGS-167]

--- src/game_card_support/handler/handler_of_2/game_card_support_2_handler_impl.rs
+++ src/game_card_support/handler/handler_of_2/game_card_support_2_handler_impl.rs
@@ -9,6 +9,10 @@ impl GameCardSupportHandler for SupportCard_2_Function {
     unsafe fn use_support_card(&self, use_support_card_request: UseSupportCardRequest) -> GameCardSupportEffect {
         println!("SupportCard_2_Function: use_support_card()");
 
-        GameCardSupportEffect::new(RaceEnum::Undead, 2)
+        // TODO: 사실 모든 것이 에너지 검색이 아니므로 이 부분 전부 setter로 수정이 필요함
+        let mut game_card_support_effect = GameCardSupportEffect::new(RaceEnum::Undead, 2);
+        game_card_support_effect.set_need_to_find_card_id(93);
+
+        return game_card_support_effect;
     }
 }
