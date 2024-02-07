2cb6dbc04abd12b297eff68d24d5c2532bd67821
Sanghoon Lee <gcccompil3r@gmail.com>
2024-02-07 17:16:29 +0900

    refactor: [이상훈] 덱에서 에너지 검색하기 [TCG-CGS_191]

--- src/game_deck/service/game_deck_service.rs
+++ src/game_deck/service/game_deck_service.rs
@@ -1,9 +1,11 @@
 use async_trait::async_trait;
+use crate::game_deck::service::request::found_card_from_deck_request::FoundCardFromDeckRequest;
 
 use crate::game_deck::service::request::game_deck_card_draw_request::GameDeckCardDrawRequest;
 use crate::game_deck::service::request::game_deck_card_list_request::GameDeckCardListRequest;
 use crate::game_deck::service::request::game_deck_card_redraw_request::GameDeckCardRedrawRequest;
 use crate::game_deck::service::request::game_deck_card_shuffled_list_request::GameDeckCardShuffledListRequest;
+use crate::game_deck::service::response::found_card_from_deck_response::FoundCardFromDeckResponse;
 use crate::game_deck::service::response::game_deck_card_draw_list_response::GameDeckCardDrawListResponse;
 use crate::game_deck::service::response::game_deck_card_redraw_response::GameDeckCardRedrawResponse;
 use crate::game_deck::service::response::game_deck_card_shuffled_list_response::GameDeckCardShuffledListResponse;
@@ -15,4 +17,5 @@ pub trait GameDeckService {
     async fn shuffle_deck(&self, game_deck_card_list_request: GameDeckCardShuffledListRequest) -> GameDeckCardShuffledListResponse;
     async fn draw_deck(&self, game_deck_card_draw_request: GameDeckCardDrawRequest) -> GameDeckCardDrawListResponse;
     async fn shuffle_and_redraw_deck(&self, game_deck_card_redraw_request: GameDeckCardRedrawRequest) -> GameDeckCardRedrawResponse;
+    async fn find_by_card_id_with_count(&self, found_card_from_deck_request: FoundCardFromDeckRequest) -> FoundCardFromDeckResponse;
 }
