660049554076601e362aab5cd4a84b2d3ca75d45
Sanghoon Lee <gcccompil3r@gmail.com>
2024-02-08 09:54:19 +0900

    refactor: [이상훈] Card Unique ID로 Card Race를 획득 [TCG-CGS-202]

--- src/game_field_unit/repository/game_field_unit_repository.rs
+++ src/game_field_unit/repository/game_field_unit_repository.rs
@@ -1,10 +1,11 @@
+use crate::common::card_attributes::card_race::card_race_enum::RaceEnum;
 use crate::game_field_unit::entity::game_field_unit_card::GameFieldUnitCard;
 use crate::game_field_unit::entity::race_enum_value::RaceEnumValue;
 
 pub trait GameFieldUnitRepository {
     fn create_game_field_unit_object(&mut self, account_unique_id: i32) -> bool;
     fn add_unit_to_game_field(&mut self, account_unique_id: i32, unit_card_number: i32) -> bool;
-    fn attach_energy_to_game_field_unit(&mut self, account_unique_id: i32, unit_card_number: i32, race: RaceEnumValue, quantity: i32);
+    fn attach_energy_to_game_field_unit(&mut self, account_unique_id: i32, unit_card_number: i32, race_enum: RaceEnum, quantity: i32);
     fn attach_multiple_energy_to_game_field_unit(&mut self, account_unique_id: i32, unit_card_number: i32, race_number: i32, quantity: i32) -> bool;
     fn find_unit_by_id(&self, account_unique_id: i32, unit_card_number: i32) -> Option<&GameFieldUnitCard>;
 }
