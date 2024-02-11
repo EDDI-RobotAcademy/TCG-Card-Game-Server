8a346576af0543caf61f9f6ff91d2525b268e12e
silenc3502 <gcccompil3r@gmail.com>
2024-02-12 01:48:00 +0900

    refactor: [이상훈] 요약기 구성 변경에 따른 일반 에너지 구성 추가 [TCG-CGS-219]

--- src/game_card_energy/handler/handler_of_93/game_card_energy_93_handler_impl.rs
+++ src/game_card_energy/handler/handler_of_93/game_card_energy_93_handler_impl.rs
@@ -1,4 +1,6 @@
 use crate::common::card_attributes::card_race::card_race_enum::RaceEnum;
+use crate::game_card_energy::entity::effect::Effect;
+use crate::game_card_energy::entity::status_effect::StatusEffect;
 use crate::game_card_energy::entity::summary_energy_card_effect::SummaryEnergyCardEffect;
 use crate::game_card_energy::handler::game_card_energy_handler::GameCardEnergyHandler;
 
@@ -8,6 +10,7 @@ impl GameCardEnergyHandler for EnergyCard_93_Function {
     unsafe fn use_specific_energy_card(&self) -> SummaryEnergyCardEffect {
         println!("SupportCard_2_Function: use_specific_support_card()");
 
-        SummaryEnergyCardEffect::new(RaceEnum::Undead)
+        SummaryEnergyCardEffect::new(RaceEnum::Undead,
+                                     vec![StatusEffect::new(Effect::Dummy, -1, -1, -1)])
     }
 }
