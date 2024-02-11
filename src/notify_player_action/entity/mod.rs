6bf73e597c9801fb97037b1c4b2577ce38d8b2be
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-02-11 14:53:04 +0900

    update: [박장훈] 검색 유형 서포트 카드 사용 공지에 대한 response type 지정 [TCG-CGS-41]

--- src/notify_player_action/entity/mod.rs
+++ src/notify_player_action/entity/mod.rs
@@ -2,3 +2,4 @@ pub mod notify_opponent_to_energy_usage;
 pub mod notify_opponent_to_energy_boost;
 pub mod notify_opponent_to_unit_deploy;
 pub mod notify_opponent_to_draw_support_usage;
+pub mod notify_opponent_search_support_usage;
