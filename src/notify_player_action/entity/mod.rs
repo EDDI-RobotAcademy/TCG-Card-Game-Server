ca83e1d0f91bc5854004f2cc1f9e3517ec517f6a
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-02-12 00:53:57 +0900

    update: [박장훈] 필드 에너지 제거 서포트 카드 사용 공지 response type 지정 [TCG-CGS-41]

--- src/notify_player_action/entity/mod.rs
+++ src/notify_player_action/entity/mod.rs
@@ -5,3 +5,4 @@ pub mod notify_opponent_to_draw_support_usage;
 pub mod notify_opponent_search_support_usage;
 pub mod notify_opponent_to_instant_death_item_alternatives_usage;
 pub mod notify_opponent_to_instant_death_item_usage;
+pub mod notify_opponent_remove_field_energy_support_usage;
