1af96d6b5fb488ac4beeebb5317ba19b280418cc
Sanghoon Lee <gcccompil3r@gmail.com>
2024-02-14 09:47:59 +0900

    update: [이상훈] Active Skill 을 Summary 할 수 있는 Handler 를 구성합니다 [TCG-CGS-260]

--- src/game_card_unit/controller/game_card_unit_controller.rs
+++ src/game_card_unit/controller/game_card_unit_controller.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
+use crate::game_card_unit::controller::request_form::attack_unit_request_form::AttackUnitRequestForm;
 use crate::game_card_unit::controller::request_form::deploy_unit_request_form::DeployUnitRequestForm;
+use crate::game_card_unit::controller::response_form::attack_unit_response_form::AttackUnitResponseForm;
 use crate::game_card_unit::controller::response_form::deploy_unit_response_form::DeployUnitResponseForm;
 
 #[async_trait]
