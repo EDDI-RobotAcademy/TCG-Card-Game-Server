184c4b6671589a4c5197e75913f779edf63a7116
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2024-02-15 17:06:31 +0900

    feat:[황영찬] 필드 유닛의 active skill 사용요청을 받으면, request form 을 생성합니다[TCG-CGS-305]

---
+++ src/request_generator/targeting_active_skill_request_form_generator.rs
@@ -0,0 +1,19 @@
+use serde_json::Value as JsonValue;
+use crate::game_card_active_skill::controller::request_form::targeting_active_skill_request_form::TargetingActiveSkillRequestForm;
+
+pub fn create_targeting_active_skill_request_form(data: &JsonValue) -> Option<TargetingActiveSkillRequestForm> {
+    if let (Some(sessionInfo), Some(unit_card_index), Some(opponent_target_card_index), Some(usage_skill_index)) = (
+        data.get("sessionInfo").and_then(|v| v.as_str()),
+        data.get("unit_card_index").and_then(|v| v.as_str()),
+        data.get("opponent_target_card_index").and_then(|v| v.as_str()),
+        data.get("usage_skill_index").and_then(|v| v.as_str()),
+    ) {
+        Some(TargetingActiveSkillRequestForm::new(
+            sessionInfo.to_string(),
+            unit_card_index.to_string(),
+            opponent_target_card_index.to_string(),
+            usage_skill_index.to_string()))
+    } else {
+        None
+    }
+}
