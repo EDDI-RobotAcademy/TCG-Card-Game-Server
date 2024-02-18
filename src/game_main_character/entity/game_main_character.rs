75c5966dc73bec8e9a70a91c032e83c892e0ceea
bikjo99 <bikjo99@gmail.com>
2024-02-19 02:11:58 +0900

    feat: [이성용] 본체 상태 enum 정의 [TCG-CGS-324]

--- src/game_main_character/entity/game_main_character.rs
+++ src/game_main_character/entity/game_main_character.rs
@@ -1,14 +1,19 @@
 use crate::game_main_character::entity::health_point::HealthPoint;
+use crate::game_main_character::entity::status_main_character::StatusMainCharacterEnum;
+use crate::game_main_character::entity::status_main_character::StatusMainCharacterEnum::Survival;
 
-#[derive(Debug)]
+
+#[derive(Debug, Clone)]
 pub struct GameMainCharacter {
-    health_point: HealthPoint
+    health_point: HealthPoint,
+    status: StatusMainCharacterEnum,
 }
 
 impl GameMainCharacter {
     pub fn new(initial_health: i32) -> GameMainCharacter {
         GameMainCharacter {
             health_point: HealthPoint::new(initial_health),
+            status: Survival,
         }
     }
 
@@ -16,17 +21,28 @@ impl GameMainCharacter {
         self.health_point.get_health()
     }
 
+    pub fn get_status(&self) -> StatusMainCharacterEnum { self.status }
+
+    pub fn set_status(&mut self, status_update: StatusMainCharacterEnum) -> StatusMainCharacterEnum {
+        self.status = status_update;
+        status_update
+    }
+
     pub fn decrease_health_point(&mut self, damage: i32) { self.health_point.lose_health(damage) }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::game_main_character::entity::status_main_character::StatusMainCharacterEnum::Death;
     use super::*;
 
     #[test]
     fn test_game_main_character() {
-        let main_character = GameMainCharacter::new(150);
+        let mut main_character = GameMainCharacter::new(150);
         println!("GameMainCharacter HealthPoint: {}", main_character.get_health_point());
+        println!("GameMainCharacter status: {:?}", main_character.get_status());
+        main_character.set_status(Death);
+        println!("GameMainCharacter status after set status: {:?}", main_character.get_status());
         assert_eq!(main_character.get_health_point(), 150);
 
         let main_character = GameMainCharacter::new(200);
