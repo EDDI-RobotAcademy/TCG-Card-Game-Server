5db33ede5cdfc677556bda70c7a3cc82e271041f
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-02-18 14:54:17 +0900

    Update targeting_active_skill_request_form_generator.rs

--- src/request_generator/targeting_active_skill_request_form_generator.rs
+++ src/request_generator/targeting_active_skill_request_form_generator.rs
@@ -4,9 +4,9 @@ use crate::game_card_active_skill::controller::request_form::targeting_active_sk
 pub fn create_targeting_active_skill_request_form(data: &JsonValue) -> Option<TargetingActiveSkillRequestForm> {
     if let (Some(sessionInfo), Some(unit_card_index), Some(opponent_target_card_index), Some(usage_skill_index)) = (
         data.get("sessionInfo").and_then(|v| v.as_str()),
-        data.get("unit_card_index").and_then(|v| v.as_str()),
-        data.get("opponent_target_card_index").and_then(|v| v.as_str()),
-        data.get("usage_skill_index").and_then(|v| v.as_str()),
+        data.get("unitCardIndex").and_then(|v| v.as_str()),
+        data.get("opponentTargetCardIndex").and_then(|v| v.as_str()),
+        data.get("usageSkillIndex").and_then(|v| v.as_str()),
     ) {
         Some(TargetingActiveSkillRequestForm::new(
             sessionInfo.to_string(),
