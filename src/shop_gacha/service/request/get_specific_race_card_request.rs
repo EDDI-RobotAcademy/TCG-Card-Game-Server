0ed23933659365babefe809b2ffde0dc361e523a
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2024-02-20 12:40:12 +0900

    feat:[황영찬] 상점에서 무료 뽑기를 할 수 있습니다.  [TCG-CGS-338]

--- src/shop_gacha/service/request/get_specific_race_card_request.rs
+++ src/shop_gacha/service/request/get_specific_race_card_request.rs
@@ -2,31 +2,17 @@ use crate::common::card_attributes::card_race::card_race_enum::RaceEnum;
 
 #[derive(Debug)]
 pub struct GetSpecificRaceCardRequest {
-    account_session_id: String,
-    race_name: String,
+    account_id: i32,
+    race_name: RaceEnum,
     is_confirmed_upper_legend: bool,
 }
 
 impl GetSpecificRaceCardRequest {
-    pub fn new(account_id: String, race_name: String, is_confirmed_upper_legend: bool) -> Self {
-        GetSpecificRaceCardRequest { account_session_id: account_id.to_string(), race_name: race_name.to_string(), is_confirmed_upper_legend }
+    pub fn new(account_id: i32, race_name: RaceEnum, is_confirmed_upper_legend: bool) -> Self {
+        GetSpecificRaceCardRequest { account_id: account_id, race_name: race_name, is_confirmed_upper_legend }
     }
-    pub fn account_session_id(&self) -> &str { &self.account_session_id }
-    pub fn get_race_enum(&self) -> RaceEnum {
-        match self.race_name.as_str() {
-            "Dummy" => RaceEnum::Dummy,
-            "Undead" => RaceEnum::Undead,
-            "Human" => RaceEnum::Human,
-            "Trent" => RaceEnum::Trent,
-            "Angel" => RaceEnum::Angel,
-            "Machine" => RaceEnum::Machine,
-            "Chaos" => RaceEnum::Chaos,
-            _ => {
-                eprintln!("Invalid race name: {}", self.race_name);
-                RaceEnum::Dummy
-            }
-        }
-    }
+    pub fn account_id(&self) -> i32 { self.account_id }
+    pub fn get_race_enum(&self) -> RaceEnum { self.race_name }
 
     pub fn is_confirmed_upper_legend(&self) -> bool { self.is_confirmed_upper_legend }
 }
