d26c093b609161a720e7f6f53b698be29d16b7f3
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2024-02-22 16:43:52 +0900

    update:[황영찬] 덱을 저장 할 때 덱에 저장할 카드를 플레이어가 가지고 있는지 검증합니다. [TCG-CGS-358]

--- src/deck_configuration_validator/service/deck_configuration_validator_service.rs
+++ src/deck_configuration_validator/service/deck_configuration_validator_service.rs
@@ -3,4 +3,5 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait DeckConfigurationValidatorService {
     async fn validate_deck(&self, deck: &Vec<i32>) -> Result<(), String>;
+    async fn do_you_have_this_card(&self, card_list: Vec<i32>, account_unique_id: i32) -> bool ;
 }
