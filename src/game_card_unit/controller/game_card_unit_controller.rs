6a1913fb0e2078bf1e51cd71aba63ee3b5c23b69
tkdrms420 <149447077+tkdrms420@users.noreply.github.com>
2024-02-23 16:06:16 +0900

    feat:[윤상근] 유닛 카드로 플레이어를 직접 공격하는 controller 구성합니다[TCG-CGS-342]

--- src/game_card_unit/controller/game_card_unit_controller.rs
+++ src/game_card_unit/controller/game_card_unit_controller.rs
@@ -1,12 +1,17 @@
 use async_trait::async_trait;
 use crate::game_card_unit::controller::request_form::attack_unit_request_form::AttackUnitRequestForm;
 use crate::game_card_unit::controller::request_form::deploy_unit_request_form::DeployUnitRequestForm;
+use crate::game_card_unit::controller::request_form::attack_game_main_character_request_form::AttackGameMainCharacterRequestForm;
 use crate::game_card_unit::controller::response_form::attack_unit_response_form::AttackUnitResponseForm;
 use crate::game_card_unit::controller::response_form::deploy_unit_response_form::DeployUnitResponseForm;
+use crate::game_card_unit::controller::response_form::attack_game_main_character_response_form::AttackGameMainCharacterResponseForm;
 
 #[async_trait]
 pub trait GameCardUnitController {
     async fn request_to_deploy_unit(
         &self, deploy_unit_request_form: DeployUnitRequestForm) -> DeployUnitResponseForm;
     async fn request_to_attack_unit(&self, attack_unit_request_form: AttackUnitRequestForm) -> AttackUnitResponseForm;
+    async fn request_to_attack_game_main_character(
+        &self, attack_game_main_character_request_form: AttackGameMainCharacterRequestForm) -> AttackGameMainCharacterResponseForm;
 }
+
