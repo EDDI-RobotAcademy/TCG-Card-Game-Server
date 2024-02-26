1c44041007ac7b75f5021bd771d3b92f95066019
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2024-02-26 17:47:29 +0900

    update:[황영찬] 나의 id와 상대의 id 를 받아오고 나의(요청한사람) 승리를 판정받게 합니다. [TCG-CGS-325]

--- src/game_winner_check/service/request/check_main_character_request.rs
+++ src/game_winner_check/service/request/check_main_character_request.rs
@@ -1,16 +1,19 @@
 #[derive(Debug)]
 pub struct CheckMainCharacterRequest {
     account_unique_id: i32,
+    opponent_unique_id: i32,
 }
 
 impl CheckMainCharacterRequest {
-    pub fn new(account_unique_id: i32) -> Self {
+    pub fn new(account_unique_id: i32, opponent_unique_id: i32) -> Self {
         CheckMainCharacterRequest {
-            account_unique_id
+            account_unique_id,
+            opponent_unique_id
         }
     }
 
     pub fn get_account_unique_id(&self) -> i32 {
         self.account_unique_id
     }
+    pub fn get_opponent_unique_id(&self) -> i32 { self.opponent_unique_id }
 }
