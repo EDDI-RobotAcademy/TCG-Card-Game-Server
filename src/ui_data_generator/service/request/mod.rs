0514b98da01a4cbd1c6ce2fadce15ca2c03d47b5
이형준 <gggudwnsgg@naver.com>
2024-02-29 10:30:44 +0900

    feat:[이형준] 상대 덱 드로우(리스트) 데이터 형성 [TCG-CGS-396]

--- src/ui_data_generator/service/request/mod.rs
+++ src/ui_data_generator/service/request/mod.rs
@@ -9,3 +9,4 @@ pub mod generate_opponent_specific_unit_health_point_data_request;
 pub mod generate_use_my_hand_card_data_request;
 pub mod generate_my_specific_unit_health_point_data_request;
 pub mod generate_opponent_multiple_unit_health_point_data_request;
+pub mod generate_draw_opponent_deck_data_request;
--- src/ui_data_generator/service/ui_data_generator_service.rs
+++ src/ui_data_generator/service/ui_data_generator_service.rs
@@ -5,6 +5,7 @@ use crate::ui_data_generator::service::request::generate_opponent_specific_unit_
 use crate::ui_data_generator::service::request::generate_use_my_hand_card_data_request::GenerateUseMyHandCardDataRequest;
 use crate::ui_data_generator::service::request::generate_use_my_deck_card_list_data_request::GenerateUseMyDeckCardListDataRequest;
 use crate::ui_data_generator::service::request::generate_draw_my_deck_data_request::GenerateDrawMyDeckDataRequest;
+use crate::ui_data_generator::service::request::generate_draw_opponent_deck_data_request::GenerateDrawOpponentDeckDataRequest;
 use crate::ui_data_generator::service::request::generate_my_specific_unit_health_point_data_request::GenerateMySpecificUnitHealthPointDataRequest;
 use crate::ui_data_generator::service::request::generate_opponent_field_energy_data_request::GenerateOpponentFieldEnergyDataRequest;
 use crate::ui_data_generator::service::request::generate_opponent_multiple_unit_health_point_data_request::GenerateOpponentMultipleUnitHealthPointDataRequest;
@@ -16,6 +17,7 @@ use crate::ui_data_generator::service::response::generate_opponent_specific_unit
 use crate::ui_data_generator::service::response::generate_use_my_hand_card_data_response::GenerateUseMyHandCardDataResponse;
 use crate::ui_data_generator::service::response::generate_use_my_deck_card_list_data_response::GenerateUseMyDeckCardListDataResponse;
 use crate::ui_data_generator::service::response::generate_draw_my_deck_data_response::GenerateDrawMyDeckDataResponse;
+use crate::ui_data_generator::service::response::generate_draw_opponent_deck_data_response::GenerateDrawOpponentDeckDataResponse;
 use crate::ui_data_generator::service::response::generate_my_specific_unit_health_point_data_response::GenerateMySpecificUnitHealthPointDataResponse;
 use crate::ui_data_generator::service::response::generate_opponent_field_energy_data_response::GenerateOpponentFieldEnergyDataResponse;
 use crate::ui_data_generator::service::response::generate_opponent_multiple_unit_health_point_data_response::GenerateOpponentMultipleUnitHealthPointDataResponse;
@@ -48,6 +50,10 @@ pub trait UiDataGeneratorService {
         &mut self,
         generate_draw_my_deck_data_request: GenerateDrawMyDeckDataRequest)
         -> GenerateDrawMyDeckDataResponse;
+    async fn generate_draw_opponent_deck_data(
+        &mut self,
+        generate_draw_opponent_deck_data_request: GenerateDrawOpponentDeckDataRequest)
+        -> GenerateDrawOpponentDeckDataResponse;
     async fn generate_search_my_deck_data(
         &mut self,
         generate_search_my_deck_data_request: GenerateSearchMyDeckDataRequest)
