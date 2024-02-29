8164fa82766e6ccaaad72c2288b42c9f33ddbef0
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-02-29 21:46:02 +0900

    refactor: [박장훈] data 기반 response form 형성 (필드 에너지 유닛 부착) [TCG-CGS-399]

--- src/game_field_energy/controller/response_form/attach_field_energy_to_field_unit_response_form.rs
+++ src/game_field_energy/controller/response_form/attach_field_energy_to_field_unit_response_form.rs
@@ -2,34 +2,34 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use crate::ui_data_generator::entity::field_unit_energy_info::FieldUnitEnergyInfo;
 use crate::ui_data_generator::entity::player_index_enum::PlayerIndex;
-use crate::notify_player_action_info::service::response::notice_use_field_energy_to_specific_unit_response::NoticeUseFieldEnergyToSpecificUnitResponse;
+use crate::ui_data_generator::service::response::generate_my_field_energy_data_response::GenerateMyFieldEnergyDataResponse;
+use crate::ui_data_generator::service::response::generate_my_specific_unit_energy_data_response::GenerateMySpecificUnitEnergyDataResponse;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AttachFieldEnergyToFieldUnitResponseForm {
     is_success: bool,
-    player_field_unit_energy_map: HashMap<PlayerIndex, FieldUnitEnergyInfo>,
     player_field_energy_map: HashMap<PlayerIndex, i32>,
+    player_field_unit_energy_map: HashMap<PlayerIndex, FieldUnitEnergyInfo>,
 }
 
 impl AttachFieldEnergyToFieldUnitResponseForm {
     pub fn new(is_success: bool,
-               player_field_unit_energy_map: HashMap<PlayerIndex, FieldUnitEnergyInfo>,
-               player_field_energy_map: HashMap<PlayerIndex, i32>) -> Self {
+               player_field_energy_map: HashMap<PlayerIndex, i32>,
+               player_field_unit_energy_map: HashMap<PlayerIndex, FieldUnitEnergyInfo>) -> Self {
         AttachFieldEnergyToFieldUnitResponseForm {
             is_success,
-            player_field_unit_energy_map,
             player_field_energy_map,
+            player_field_unit_energy_map,
         }
     }
-    pub fn from_response(notice_use_field_energy_to_specific_unit_response: NoticeUseFieldEnergyToSpecificUnitResponse) -> AttachFieldEnergyToFieldUnitResponseForm {
+    pub fn from_response(generate_my_field_energy_data_response: GenerateMyFieldEnergyDataResponse,
+                         generate_my_specific_unit_energy_data_response: GenerateMySpecificUnitEnergyDataResponse
+    ) -> AttachFieldEnergyToFieldUnitResponseForm {
+
         AttachFieldEnergyToFieldUnitResponseForm::new(
             true,
-            notice_use_field_energy_to_specific_unit_response
-                .get_player_field_unit_energy_info()
-                .get_player_field_unit_energy_map().clone(),
-            notice_use_field_energy_to_specific_unit_response
-                .get_player_field_energy_info()
-                .get_player_field_energy_map().clone())
+            generate_my_field_energy_data_response.get_player_field_energy_map_for_response().clone(),
+            generate_my_specific_unit_energy_data_response.get_player_field_unit_energy_map_for_response().clone())
     }
 
     pub fn default() -> AttachFieldEnergyToFieldUnitResponseForm {
