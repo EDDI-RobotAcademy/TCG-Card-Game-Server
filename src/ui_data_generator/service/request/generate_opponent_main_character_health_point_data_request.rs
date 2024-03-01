335516d430ee060006c3baaa48070e045f530b64
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-03-02 01:23:47 +0900

    update: [박장훈] 상대 본체 체력 데이터 생성 request 수정 [TCG-CGS-396]

--- src/ui_data_generator/service/request/generate_opponent_main_character_health_point_data_request.rs
+++ src/ui_data_generator/service/request/generate_opponent_main_character_health_point_data_request.rs
@@ -1,17 +1,14 @@
 #[derive(Debug)]
 pub struct GenerateOpponentMainCharacterHealthPointDataRequest {
-    main_character_index:i32,
     main_character_updated_health_point: i32,
 }
 
 impl GenerateOpponentMainCharacterHealthPointDataRequest {
-    pub fn new(main_character_index:i32,main_character_updated_health_point: i32) -> Self {
+    pub fn new(main_character_updated_health_point: i32) -> Self {
         GenerateOpponentMainCharacterHealthPointDataRequest {
-            main_character_index,
             main_character_updated_health_point
         }
     }
 
-    pub fn get_main_character_index(&self) -> i32 { self.main_character_index }
     pub fn get_main_character_updated_health_point(&self) -> i32 { self.main_character_updated_health_point }
 }
