0d2f75a6f85a259d6931b6a3ebc4395b9b80dc1f
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2024-03-04 02:14:52 +0900

    feat:[황영찬] 턴 시작 시 사용되는 패시브 스킬을 확인 [TCG-CGS-403]

--- src/game_field_unit_action_possibility_validator/service/game_field_unit_action_possibility_validator_service.rs
+++ src/game_field_unit_action_possibility_validator/service/game_field_unit_action_possibility_validator_service.rs
@@ -2,9 +2,11 @@ use async_trait::async_trait;
 use crate::game_field_unit_action_possibility_validator::service::request::is_unit_basic_attack_possible_request::IsUnitBasicAttackPossibleRequest;
 use crate::game_field_unit_action_possibility_validator::service::request::is_using_active_skill_possible_request::IsUsingActiveSkillPossibleRequest;
 use crate::game_field_unit_action_possibility_validator::service::request::is_using_deploy_passive_skill_possible_request::IsUsingDeployPassiveSkillPossibleRequest;
+use crate::game_field_unit_action_possibility_validator::service::request::is_using_turn_start_passive_skill_request::IsUsingTurnStartPassiveSkillPossibleRequest;
 use crate::game_field_unit_action_possibility_validator::service::response::is_unit_basic_attack_possible_response::IsUnitBasicAttackPossibleResponse;
 use crate::game_field_unit_action_possibility_validator::service::response::is_using_active_skill_possible_response::IsUsingActiveSkillPossibleResponse;
 use crate::game_field_unit_action_possibility_validator::service::response::is_using_deploy_passive_skill_possible_response::IsUsingDeployPassiveSkillPossibleResponse;
+use crate::game_field_unit_action_possibility_validator::service::response::is_using_turn_start_passive_skill_response::IsUsingTurnStartPassiveSkillPossibleResponse;
 
 #[async_trait]
 pub trait GameFieldUnitActionPossibilityValidatorService {
@@ -18,4 +20,7 @@ pub trait GameFieldUnitActionPossibilityValidatorService {
     async fn is_using_deploy_passive_skill_possible(
         &self, is_using_deploy_passive_skill_possible_request: IsUsingDeployPassiveSkillPossibleRequest)
         -> IsUsingDeployPassiveSkillPossibleResponse;
+    async fn is_using_turn_start_passive_skill_possible(
+        &self, is_using_turn_start_passive_skill_possible_request: IsUsingTurnStartPassiveSkillPossibleRequest)
+        -> IsUsingTurnStartPassiveSkillPossibleResponse;
 }
--- src/game_field_unit_action_possibility_validator/service/request/is_using_turn_start_passive_skill_request.rs
+++ src/game_field_unit_action_possibility_validator/service/request/is_using_turn_start_passive_skill_request.rs
@@ -4,21 +4,21 @@ use crate::game_card_passive_skill::entity::passive_skill_casting_condition::Pas
 use crate::game_card_passive_skill::entity::passive_skill_type::PassiveSkillType;
 
 #[derive(Debug)]
-pub struct IsUsingDeployPassiveSkillPossibleRequest {
+pub struct IsUsingTurnStartPassiveSkillPossibleRequest {
     account_unique_id: i32,
     field_unit_index: i32,
     passive_skill_index: i32,
     passive_skill_casting_condition: Vec<PassiveSkillCastingCondition>
 }
 
-impl IsUsingDeployPassiveSkillPossibleRequest {
+impl IsUsingTurnStartPassiveSkillPossibleRequest {
     pub fn new(account_unique_id: i32,
                field_unit_index: i32,
                passive_skill_index: i32,
                passive_skill_casting_condition: Vec<PassiveSkillCastingCondition>
 
     ) -> Self {
-        IsUsingDeployPassiveSkillPossibleRequest {
+        IsUsingTurnStartPassiveSkillPossibleRequest {
             account_unique_id,
             field_unit_index,
             passive_skill_index,
