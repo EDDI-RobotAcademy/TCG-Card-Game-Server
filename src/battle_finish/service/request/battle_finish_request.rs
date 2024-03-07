a812f253f528a33426a45344872100ac5662d0ba
bikjo99 <bikjo99@gmail.com>
2024-03-07 14:39:49 +0900

    update: [이성용] request_generator 인자에 Enum 추가 [TCG-CGS-367]

--- src/battle_finish/service/request/battle_finish_request.rs
+++ src/battle_finish/service/request/battle_finish_request.rs
@@ -3,13 +3,14 @@ use crate::game_winner_check::entity::finish_position_enum::FinishPositionEnum;
 #[derive(Debug)]
 pub struct BattleFinishRequest {
     session_id: String,
-    // finish_position: FinishPositionEnum
+    finish_position: FinishPositionEnum
 }
 
 impl BattleFinishRequest {
-    pub fn new(session_id: String) -> Self {
+    pub fn new(session_id: String, finish_position: FinishPositionEnum) -> Self {
         BattleFinishRequest {
             session_id: session_id.to_string(),
+            finish_position,
         }
     }
 
@@ -17,5 +18,5 @@ impl BattleFinishRequest {
         &self.session_id
     }
 
-    // pub fn get_finish_position(&self) -> &FinishPositionEnum { &self.finish_position }
+    pub fn get_finish_position(&self) -> &FinishPositionEnum { &self.finish_position }
 }
