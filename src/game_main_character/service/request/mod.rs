1f946a8aee89ac64f927e773fca48ee95a3d23e2
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-03-07 14:58:35 +0900

    feat: [박장훈] 덱사가 발생한 경우에도 플레이어를 사망 상태로 세팅할 수 있습니다 [TCG-CGS-434]

--- src/game_main_character/service/request/mod.rs
+++ src/game_main_character/service/request/mod.rs
@@ -1,3 +1,4 @@
 pub mod apply_damage_to_main_character_request;
 pub mod check_main_character_of_account_unique_id_request;
 pub mod get_current_main_character_health_point_request;
+pub mod set_main_character_as_death_request;
