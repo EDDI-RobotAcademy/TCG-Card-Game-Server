1954a991ac1b52e7ff6a0c2b7f94625cdde61617
bikjo99 <bikjo99@gmail.com>
2024-03-24 00:45:21 +0900

    feat: [이성용] deck_id 검색 기능 [TCG-CGS-448]

--- src/account_deck/repository/account_deck_repository.rs
+++ src/account_deck/repository/account_deck_repository.rs
@@ -10,6 +10,7 @@ pub trait AccountDeckRepository {
     async fn save(&self, deck: AccountDeck) -> Result<(), Error>;
     async fn get_account_deck_id_list(&self, account_unique_id: i32) -> Result<Vec<i32>, Error>;
     async fn get_list_by_user_int_id(&self, request: i32) -> Result<Option<Vec<HashMap<i32, String>>>, Error>;
+    async fn find_deck_id_by_deck_name(&self, account_id: i32, deck_name: &str) -> i32;
     async fn update(&self, modify_deck: AccountDeckModifyRequest, int_id: i32) -> Result<(), Error>;
     async fn delete(&self, deck_id: i32) -> Result<(), Error>;
     async fn delete_all_account_decks(&self, account_unique_id: i32) -> Result<(), Error>;
